c2b861040f9618c5e0d75790f0fed92d72eaf126
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-06-27 16:08:20 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -25,19 +25,19 @@ correctLink.addEventListener('click', () => {
   removeStylesheet('wrong');
   addStylesheet('./src/correct.css', 'correct');
   app.replaceChild(correctHTML, wrongHTML);
+
+  // Uncomment this function invocation below
+  // part1();
 });
 
 wrongLink.addEventListener('click', () => {
   removeStylesheet('correct');
   addStylesheet('./src/wrong.css', 'wrong');
   app.replaceChild(wrongHTML, correctHTML);
+
+  // Uncomment this function invocation below
+  // part2();
 });
-
-// Uncomment this function invocation below
-// part1();
-
-// Uncomment this function invocation below
-// part2();
 
 // ######### Hoisted functions below ##########
 
