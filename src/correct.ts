86c0d6d2a198418654bfa87c77d8589360c209bc
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-06-27 15:53:47 +0200

    initial commit

---
+++ src/correct.ts
@@ -0,0 +1,76 @@
+import cheesecake from './assets/frozen-cheesecake-slice.jpg';
+
+export const correctHTML = document.createElement('main');
+
+correctHTML.id = "correct-page"
+
+correctHTML.innerHTML = /*html*/ `
+  <section class="introduction">
+    <article class="description-container">
+      <h1 id="recipe-name">Frozen Cheescake</h1>
+      <div class="time-container">
+        <span class="material-icons"> av_timer </span><span class="time">60+ min</span>
+      </div>
+      <p class="description">
+        Fryst cheesecake är en perfekt dessert för stora kalas eller middagar eftersom du kan
+        göra den i förväg och frysa in. Ta fram cheesecaken en stund innan servering och garnera
+        med dina favoritfrukter och bär.
+      </p>
+    </article>
+    <article class="image-container">
+      <img src="${cheesecake}" alt="Slice of Frozen Cheescake" />
+    </article>
+  </section>
+  <section class="how-to-do">
+    <article class="ingredients-container">
+      <h3 class="ingredients">Ingridienser</h3>
+      <h4 class="to-bottom">Till botten</h4>
+      <ul class="ingredients-list-bottom">
+        <li>15st digistivetex</li>
+        <li>Lite smör</li>
+      </ul>
+      <h4 class="to-paste">Till smeten</h4>
+      <ul class="ingredients-list-paste">
+        <li>3st ägg</li>
+        <li>1,5dl strösocker</li>
+        <li>3tsk vaniljsocker</li>
+        <li>3dl vispgrädde</li>
+        <li>400g naturell philadelphiaost</li>
+      </ul>
+    </article>
+    <article class="instructions-container">
+      <h3 class="instructions">Instruktioner</h3>
+      <ol class="instructions-list">
+        <li>
+          Smula sönder 10 stycken digistivetex och blanda sen med smöret. Bred ut det i botten
+          av en en rund form med lösbara kanter. Det ska bli som en tjockt täcke på botten av
+          formen.
+        </li>
+        <li>
+          Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en
+          liten bunke.
+        </li>
+        <li>
+          Vispa äggvitorna hårt med en elvisp. De är tillräckligt vispade när du kan vända
+          uppochner på bunken utan att det rör sig.
+        </li>
+        <li>
+          Häll upp grädden i en mellanstor bunke. Vispa de relativ hårt. En aning hårdare en
+          vanligt.
+        </li>
+        <li>Blanda i philadelphiaosten och vaniljsockret med äggulorna och vispa ihop.</li>
+        <li>
+          Häll nu över innehållet i bunken med äggulor över grädden. Vispa ihop på lägsta
+          hastigheten.
+        </li>
+        <li>
+          Häll sedan över det innehållet till bunken med äggvitorna. Blande ihop med en gaffel
+          tills det blir en slät fluffig smet.
+        </li>
+        <li>Häll smeten i formen, smula över de resterande digistivetexen om du vill.</li>
+        <li>Ställ in i frysen över natten.</li>
+        <li>Servera och toppa med det du känner för.</li>
+      </ol>
+    </article>
+  </section>
+`;
---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import { correctHTML } from './correct';
+import { wrongHTML } from './wrong';
+
+addStylesheet('./src/correct.css', 'correct');
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app.insertAdjacentHTML(
+  'afterbegin',
+  `<header>
+    <p class="logo-text">Niklas smaskisrecept</p>
+    <nav>
+      <a href="#" class="correct-link">Correct Version</a>
+      <a href="#" class="wrong-link">
+        Wrong Version
+      </a>
+    </nav>
+  </header>`
+);
+app.append(correctHTML);
+
+const correctLink = document.querySelector<HTMLSpanElement>('.correct-link')!;
+const wrongLink = document.querySelector<HTMLSpanElement>('.wrong-link')!;
+
+correctLink.addEventListener('click', () => {
+  removeStylesheet('wrong');
+  addStylesheet('./src/correct.css', 'correct');
+  app.replaceChild(correctHTML, wrongHTML);
+});
+
+wrongLink.addEventListener('click', () => {
+  removeStylesheet('correct');
+  addStylesheet('./src/wrong.css', 'wrong');
+  app.replaceChild(wrongHTML, correctHTML);
+});
+
+// Uncomment this function invocation below
+// part1();
+
+// Uncomment this function invocation below
+// part2();
+
+// ######### Hoisted functions below ##########
+
+function addStylesheet(href: string, id: string) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = href;
+  if (id) link.id = id;
+  document.head.appendChild(link);
+}
+
+function removeStylesheet(id: string) {
+  const existing = document.getElementById(id);
+  if (existing && existing.tagName === 'LINK') {
+    document.head.removeChild(existing);
+  }
+}
